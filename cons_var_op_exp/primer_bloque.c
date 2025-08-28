1b995e83ef8677c94f24b7b1692740c591cc0a2a
Stefano <FoodStef@gmail.com>
2025-08-28 17:30:59 -0300

    primer bloque practica completo. Hice algunos ejercicios, no todos

---
+++ cons_var_op_exp/primer_bloque.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#define DOLAR 1250
+#define VALOR 4000
+#define FIESTA "7 de julio \"San Fermin\""
+
+/*1- Definir la constante simbólica DOLAR
+dándole el valor actual de la moneda.
+
+void main(){
+    printf("El valor actual de la moneda es %d\n",DOLAR);
+}
+*/
+
+/*¿Cuál o cuáles de las siguientes
+sentencias son válidas en lenguaje C teniendo en cuenta
+que VALOR es una constante simbólica numérica entera?
+
+void main(){
+    printf("Este es el valor de la constante simbolica numerica entera %d\n",VALOR);
+}
+*/
+
+/*4
+void main(){
+    printf("%s",FIESTA);
+}
+*/
+
+/*12
+
+void main(){
+    int exp;
+    exp = !(3 > 3 && 5!=4 || 9 == 9) ;
+    printf("Primer expresion %d\n",exp);
+
+    exp = !( 7>8) && 7 >8 || 7> 8  ;
+    printf("Segunda expresion %d\n",exp);
+
+    exp = 13/13*2*5+5%2 ;
+    printf("Tercera expresion %d\n",exp);
+}
+*/
+
+/*16
+
+void main(){
+    int x = 5;
+    int result;
+
+    result = ++x + x;
+
+    printf("Resultados de la primera operacion: %d y %d", result,x);
+
+    x=5;
+
+    result = x + ++x;
+    printf("Resultados de la primera operacion: %d y %d", result,x);
+
+    //NO importa el lugar del sumando, siempre primero el incremento o decremento.
+}
+*/
+
+/*20- Escribir una instrucción donde dada una variable,
+se imprima la letra “n” si el número contenido en la variable
+es < 0  y  “Z” si el número es >= 0.
+
+void main(){
+    int var;
+    var = -21;
+
+    var < 0 ? printf("n\n"): printf("Z\n");
+}
+*/
+
+/*22- Escribir en una sola instrucción donde se pueda asignar
+a la variable p  un 1 si p es impar o un 0 si p es par,
+esto en el caso de que p sea mayor que 20, sino asignarle un 6.
+
+void main(){
+    int p;
+    p=3;
+
+    p = p>20 ? p%2 : 6;
+
+    printf("Este es el valor de p: %d\n",p);
+
+
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
