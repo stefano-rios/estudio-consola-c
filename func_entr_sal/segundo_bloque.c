03e2643bc9a6261bb0e18b29d4bbcba2831030fe
Stefano <FoodStef@gmail.com>
2025-09-06 16:37:42 -0300

    Algunos ejercicios de funciones de entrada y salida

---
+++ func_entr_sal/segundo_bloque.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/*23 x de tipo caracter e y
+de tipo entera, separadas por una / (barra)
+
+void main(){
+    char x;
+    int y;
+    printf("Ingrese dos variables separadas por una barra\nLa primera char y la segunda entera\n");
+    scanf("%c/%d",&x,&y);
+    printf("Sus variables son: %c y %d",x,y);
+}
+*/
+
+/*25- Escribir un programa que
+ingrese un carácter y muestre en pantalla el número
+decimal que le corresponde en la tabla ASCII.
+
+void main(){
+    char c;
+    printf("Ingrese un caracter y se lo mostraremos en ASCII\n");
+    scanf("%c",&c);
+    printf("Su caracter era el %c y en ASCII tiene un valor de %d",c,c);
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
