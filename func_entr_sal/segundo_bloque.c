b4475fd72158d98b8ef0225d43ef5a5cc92a98a5
Stefano <FoodStef@gmail.com>
2025-09-06 18:24:40 -0300

    Mas ejercicios de funciones de entrada y salida

--- func_entr_sal/segundo_bloque.c
+++ func_entr_sal/segundo_bloque.c
@@ -24,6 +24,43 @@ void main(){
 }
 */
 
+/*28- Escribir un programa que ingrese un entero
+y muestre en forma tabulada el número, su
+cuadrado y su cubo.
+
+void main(){
+    int ent,cuad,cub;
+    printf("Ingrese un entero, por favor\n");
+    scanf("%d",&ent);
+
+    cuad = ent*ent;
+    cub = cuad*ent;
+
+    printf("Su numero es: %d, el cuadrado es: %d, y su cubo es: %d\n",ent,cuad,cub);
+}
+*/
+
+/*30- Escribe un programa que borre
+la pantalla y escriba en la primera línea su nombre y en la
+segunda su apellido:
+
+void main(){
+    char nom[15];
+    char ape[15];
+    printf("Ingrese su nombre y apellido seguidos de un enter\n");
+    fflush(stdin);
+    gets(nom);
+    fflush(stdin);
+    gets(ape);
+    printf("Toque una tecla para limpiar pantalla...");
+    fflush(stdin);
+    getche();
+    system("cls");
+    printf("Su nombre es %s y su apellido es %s\n",nom,ape);
+
+}
+*/
+
 
 
 
