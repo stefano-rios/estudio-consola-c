a6f87dd933d2b5effbac038803a4abe8d5647611
Stefano <FoodStef@gmail.com>
2025-09-07 19:18:13 -0300

    Comienzo de sentencias condicionales

---
+++ condicionales/ejemplo_if.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*Ingresamos dos numeros, debe decirme cual es el mayor o si
+ambos son iguales
+
+void main(){
+    int p,s;
+    printf("Ingrese dos numeros separados por un espacio\n");
+    scanf("%d %d",&p,&s);
+
+    if(p>s)printf("El primer valor <%d> es mayor",p);
+    else if(p==s)printf("Los numeros <%d> y <%d> son iguales",p,s);
+    else printf("El segundo valor <%d> es mayor",s);
+
+}
+*/
---
+++ condicionales/ejemplo_switch.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/*Decirme si ingrese una consonante, una vocal, o un punto
+
+void main(){
+    char c;
+    printf("Ingrese una letra o un punto\n");
+    fflush(stdin);
+    c=getchar();
+
+    switch (c){
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u': printf("Usted ingreso una vocal <%c>",c);
+              break;
+    case '.': printf("Usted ingreso un punto <%c>",c);
+              break;
+    default:  printf("Usted ingreso una consonante <%c>",c);
+    }
+
+}
+*/
+
+
