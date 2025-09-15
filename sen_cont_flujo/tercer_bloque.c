694a028add3a06f83179838e5fcd4df9ae36a260
Stefano <FoodStef@gmail.com>
2025-09-14 22:11:30 -0300

    Otro ejercicio mas

--- sen_cont_flujo/tercer_bloque.c
+++ sen_cont_flujo/tercer_bloque.c
@@ -69,8 +69,24 @@ void main(){
 
     */
 
+    /*38- Mostrar una tabla que contenga los valores de peso
+    y costo, variando el peso desde 2
+    hasta 20 con un incremento de 6, si el costo mínimo es
+    de $50 y se incrementa en $15 y
+    cada 6 kg. Utilizar un bucle for y constantes simbólicas donde
+    se considere útil.
 
+    int costoMin, pesoMin;
 
+    int y=50 ; //costMin
+    int x=2 ; //pesoMin
+
+    printf("Mostraremos los costos por su peso en la tabla:\n");
+
+    for(x,y ; y<96 ; x+=6, y+=15){
+        printf("Peso = <%d>\tCosto = <%d>\n",x,y);
+    }
+    */
 
 
 
